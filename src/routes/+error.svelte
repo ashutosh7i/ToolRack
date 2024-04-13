7de6e259959a4921b22fecf01fc08be173e8e016
Aashutosh soni <ashutoshsoni130@gmail.com>
2024-04-13 20:41:45 +0530

    added svelte-vercel adaper for deployment

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { Heading, P } from 'flowbite-svelte';
+</script>
+
+<div class="w-full h-full flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-900">
+    <div class="flex flex-col items-center justify-center bg-white dark:bg-gray-800 p-6 rounded shadow">
+        <Heading tag="h2" class="text-gray-900 dark:text-gray-100">404 Not Found</Heading>
+        <P class="text-center text-gray-900 dark:text-gray-100">
+            The page you're looking for doesn't exist.
+        </P>
+    </div>
+</div>
